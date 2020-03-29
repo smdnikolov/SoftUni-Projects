d2878f12ad56d6fb3c5b8719bdb362aa7aa1618d
smdnikolov <smdnikolov@gmail.com>
2020-03-29 15:13:50 +0300

    test

---
+++ 8memes-spa-vue.js/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+       <app-navigation />
+ 
+  </div>
+</template>
+
+<script>
+import appNavigation from './components/app-navigation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    appNavigation
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 8memes-spa-vue.js/src/components/app-navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navigation-wrapper">
+  <nav>
+    <a>Home</a>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appNavigation',
+  props: {
+  
+  }
+}
+</script>
+
+
+<style scoped>
+.navigation-wrapper {
+  background:black;
+  color:white;
+  height: 50px;
+  text-align: center;
+}
+</style>
